39e9c28c2fc7eb5addb4194d626a1601977748b3
Swathi Chitta <chittaswathi98@gmail.com>
2024-08-26 20:18:46 -0400

    Unequal men and women (n*m)

--- stable_marriage.py
+++ stable_marriage.py
@@ -20,6 +20,7 @@ def gale_shapley_algorithm(men_pref, women_pref):
     free_men = [i for i in men_pref]
     men_proposal = {i:[] for i in men_pref}
     women_partner = {i:None for i in women_pref}
+    print(men_proposal, women_partner)
 
     while free_men:
         man = free_men.pop(0)
@@ -36,10 +37,10 @@ def gale_shapley_algorithm(men_pref, women_pref):
                         women_partner[woman] = man
                         break
     
-    return {i:j for j,i in women_partner.items()}
+    return {i:j for j,i in women_partner.items() if i}
      
 
-# Example usage:
+# Equal men to women count:
 men_pref = {
     'a': ['A', 'B', 'C', 'D'],
     'b': ['A', 'C', 'B', 'D'],
@@ -54,5 +55,40 @@ women_pref = {
     'D': ['b', 'c', 'a', 'd']
 }
 
+
+# more men, less women
+men_pref1 = {
+    'a': ['A', 'B', 'C', 'D'],
+    'b': ['A', 'C', 'B', 'D'],
+    'c': ['B', 'C', 'A', 'D'],
+    'd': ['A', 'B', 'D', 'C'],
+    'e': ['D', 'B', 'D', 'C']
+}
+
+women_pref1 = {
+    'A': ['d', 'c', 'a', 'b','e'],
+    'B': ['a', 'd', 'e', 'b', 'c'],
+    'C': ['d', 'e', 'b', 'c', 'a'],
+    'D': ['e', 'b', 'c', 'a', 'd']
+}
+
+# less men, more women
+men_pref2 = {
+    'a': ['A', 'B', 'C', 'D'],
+    'b': ['D', 'C', 'B', 'E'],
+    'c': ['B', 'C', 'E', 'D'],
+    'd': ['E', 'B', 'D', 'C'],
+}
+
+women_pref2 = {
+    'A': ['d', 'c', 'a', 'b'],
+    'B': ['a', 'd', 'b', 'c'],
+    'C': ['d', 'b', 'c', 'a'],
+    'D': ['b', 'c', 'a', 'd'],
+    'E': ['c', 'd', 'b', 'a']
+}
+
 result = gale_shapley_algorithm(men_pref, women_pref)
-print(result)
+result1 = gale_shapley_algorithm(men_pref1, women_pref1)
+result2 = gale_shapley_algorithm(men_pref2, women_pref2)
+print('Equal men and women', result,'\nMore men less women',result1, '\nLess men more women', result2)
